11ef55335c3214caa5d0fe82dc0185a7412e9724
MartijnSnijder <martijn-snijder@hotmail.com>
2016-10-26 11:57:24 +0200

    Dashboard initial commit

---
+++ dashboard.py
@@ -0,0 +1,8 @@
+import tkinter
+top = tkinter.Tk()
+
+# Hier komt code voor widgets
+top.mainloop()
+
+# Gebruik pygubu om GUI te maken in tkinter
+# Git link: https://github.com/alejandroautalan/pygubu
