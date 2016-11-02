b0dc6efde20ebe1affc394427dda885ad4a49e13
MartijnSnijder <martijn-snijder@hotmail.com>
2016-11-02 11:00:15 +0100

    Dashboard update

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,22 @@
-import tkinter
-top = tkinter.Tk()
+import tkinter as tk
+import pygubu
 
-# Hier komt code voor widgets
-top.mainloop()
 
-# Gebruik pygubu om GUI te maken in tkinter
-# Git link: https://github.com/alejandroautalan/pygubu
+class Application:
+    def __init__(self, master):
+
+        # Maak builder
+        self.builder = builder = pygubu.Builder()
+
+        # Laad GUI bestand
+        ## GUI is gemaakt met pygubu, is .ui bestand in project directory
+        builder.add_from_file('GUI.ui')
+
+        # Maak widget mbv master als 'parent'
+        self.mainwindow = builder.get_object('Toplevel_1', master)
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Application(root)
+    root.mainloop()
