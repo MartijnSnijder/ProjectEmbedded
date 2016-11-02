053ccceb48856a9e539433e3db6313f3903d2a61
MartijnSnijder <martijn-snijder@hotmail.com>
2016-11-02 12:06:41 +0100

    Slider added

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,22 @@
-import Tkinter
-top = Tkinter.Tk()
+from tkinter import *
+root = Tk()
 
-# Widget code
+class mainframe():
+    def __init__(self, parent):
+        self.parent = parent
+        self.initUI()
 
+    def initUI(self):
+        #Slider widget voor scherm uitrol
+        var = DoubleVar()
+        scale = Scale(root, from_=0, to_=160, variable = var, orient_=HORIZONTAL,
+                      tickinterval_=20, length_=300, label="Zonnescherm handmatig in/uitrollen (cm)")
+        scale.pack()
 
-# Start tkinter
-top.mainloop()
+def main():
+    root.title("Dashboard")
+    app = mainframe(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
