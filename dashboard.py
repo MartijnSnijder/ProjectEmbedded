e0c58a7e4b4719b4f53df9955ff67cdfce36269b
MartijnSnijder <martijn-snijder@hotmail.com>
2016-11-02 11:39:40 +0100

    Dashboard update: - Stopped working with pygubu. Empty tkinter file

--- dashboard.py
+++ dashboard.py
@@ -1,23 +1,8 @@
-import tkinter as tk
-import pygubu
+import Tkinter
+top = Tkinter.Tk()
+
+# Widget code
 
 
-class Application:
-    def __init__(self, master):
-
-        # Maak builder
-        self.builder = builder = pygubu.Builder()
-
-        # Laad GUI bestand
-        ## GUI is gemaakt met pygubu, is .ui bestand in project directory
-        builder.add_from_file('GUI.ui')
-
-        # Maak widget mbv master als 'parent'
-        self.mainwindow = builder.get_object('mainframe', master)
-
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    root.title("Dashboard 'De Centrale'")
-    app = Application(root)
-    root.mainloop()
+# Start tkinter
+top.mainloop()
