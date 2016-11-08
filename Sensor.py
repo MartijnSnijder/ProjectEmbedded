3e61bd380348dd0e240f5e44e13e81853d5f62b3
MartijnSnijder <martijn-snijder@hotmail.com>
2016-11-08 13:03:54 +0100

    dashboard update

--- Sensor.py
+++ Sensor.py
@@ -3,29 +3,23 @@ import time
 
 
 class connect:
-    # Deze functie zet een waarde om naar een int
-    def to_int(s):
-        try:
-            return int(s)
-        except ValueError:
-            return float(s)
-
     # Hier wordt de Arduino afgelezen en de byte wordt in "ser" gezet
+    def light(self):
         ser = serial.Serial('COM3', 9600, timeout=0)
 
         datalist = []
         while 1:
             try:
                 for line in ser:
-                    intvalue = to_int(line.rstrip().decode('utf-8'))  # Byte > Str > Int
+                    intvalue = int(line.rstrip().decode('utf-8'))  # Byte > Str > Int
                     datalist.append(intvalue)  # De int wordt in een lijst gezet
 
                     # Het gemiddele van de laatste 10 waarden in de lijst wordt geprint
                     average = sum(datalist[-10:]) / len(datalist[-10:])
-                    print(round(average))
+                    return round(average)
 
-                time.sleep(1)
+                    time.sleep(1)
 
             except ser.SerialTimeoutException:
-                print('Data could not be read')
-            time.sleep(1)
+                return('Data could not be read')
+                time.sleep(1)
