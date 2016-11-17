238ad9f9d1482963f0c38e984298b19dbe7715f1
Ryan Flierman <r.w.flierman@st.hanze.nl>
2016-11-17 10:26:49 +0100

    graph in dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,19 @@
 import tkinter as Tk
 import instellingen
 
+import matplotlib
+matplotlib.use('TkAgg')
+
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+import sys
+if sys.version_info[0] < 3:
+    import Tkinter as Tk
+else:
+    import tkinter as Tk
+
 ########################################################################
 class MyApp(object):
     """"""
@@ -11,32 +24,31 @@ class MyApp(object):
         self.root = parent
         self.root.title("Scherm overzicht")
         self.frame = Tk.Frame(parent)
-        self.frame.grid()
-        self.initUI()
-        self.buttons()
-        self.waardes()
 
+        #self.frame.grid()
+        self.line_graph()
+
+       # self.initUI()
+       # self.current_values()
 
     # ----------------------------------------------------------------------
     def hide(self):
         self.root.withdraw()
 
-    def waardes(self):
-        waardes = Tk.Label(root, text="Gekkigheid")
-        waardes.grid(row=2, column=1)
-
     def buttons(self):
-        btn_ins = Tk.Button(self.frame, text="Instellingen", command=self.openInstellingen)
-        btn_ins.grid(row=1, column=3)
+        button = Tk.Button(master=root, text='Quit', command=sys.exit)
+        button.pack(side=Tk.LEFT)
+        btn_ins = Tk.Button(master=root, text="Instellingen", command=self.openInstellingen, width=15)
+        btn_ins.pack(side=Tk.LEFT)
 
-        btn_din = Tk.Button(self.frame, text="dingen")
-        btn_din.grid(row=1, column=4)
+        btn_din = Tk.Button(master=root, text="dingen")
+        btn_din.pack(side=Tk.LEFT)
 
-        btn_inr = Tk.Button(self.frame, text="Volledig inrollen" , command=())
-        btn_inr.grid(row=4, column= 4, columnspan= 3)
+        btn_inr = Tk.Button(master=root, text="Volledig inrollen", command=(), width=15)
+        btn_inr.pack(side=Tk.LEFT)
 
-        btn_inr = Tk.Button(self.frame, text="Volledig uitrollen" , command=())
-        btn_inr.grid(row=4, column=1, columnspan=3)
+        btn_inr = Tk.Button(master=root, text="Volledig uitrollen", command=(), width=15)
+        btn_inr.pack(side=Tk.LEFT)
 
     # ----------------------------------------------------------------------
     def openInstellingen(self):
@@ -49,17 +61,49 @@ class MyApp(object):
         self.root.update()
         self.root.deiconify()
 
+    def current_values(self):
+        lijst = instellingen.instellingen.changed_fields
+        namen = instellingen.instellingen.namen
+        Tk.Label(root, text = "Huidige ingestelde waardes:").grid(row=2, column = 2)
+        y=3
+        for x in range(len(lijst)):
+            Tk.Label(root,text=namen[x], width = 25).grid(row=y, column = 1)
+            Tk.Label(root, text=lijst[x], width = 15).grid(row=y, column=2)
+            y+=1
+
     def initUI(self):
         #Slider widget voor scherm uitrol
         var = Tk.DoubleVar()
         scale = Tk.Scale(root, from_=0, to_=160, variable_=var, orient_=Tk.HORIZONTAL,
-                      tickinterval_=20, length_=300, label="Zonnescherm handmatig in/uitrollen (cm)")
-        scale.grid(row=1, column=3, columnspan= 6)
+                      tickinterval_=20, length_=200, label="Zonnescherm handmatig in/uitrollen (cm)")
+        scale.grid(row = 4, column= 8)
+
+    def line_graph(self):
+        # http://matplotlib.org/examples/user_interfaces/embedding_in_tk2.html
+
+        f = Figure(figsize=(5, 4), dpi=100)
+        a = f.add_subplot(111)
+        t = arange(0.0, 3.0, 0.01)
+        s = sin(2 * pi * t)
+
+        a.plot(t, s)
+        a.set_title('Tk embedding')
+        a.set_xlabel('X axis label')
+        a.set_ylabel('Y label')
+
+        # a tk.DrawingArea
+        canvas = FigureCanvasTkAgg(f, master=root)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+
+        canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+
+        self.buttons()
 
 
 # ----------------------------------------------------------------------
 if __name__ == "__main__":
     root = Tk.Tk()
-    root.geometry("600x200")
+    root.geometry("600x600")
     app = MyApp(root)
     root.mainloop()
